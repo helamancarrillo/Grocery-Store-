8f7e22358f4534b760b10c97d7fc0d5c287ebcbf
helamancarrillo <helamancarrillo11@gmail.com>
2024-10-24 16:53:18 -0600

    lot of nothing

--- index.js
+++ index.js
@@ -50,3 +50,6 @@ app.get('/cart', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+
